007877ee66e2d1b6859f51386b3246597fb89e6a
venki-devs <venkateswarlu.kolagani@live.com>
2022-12-28 00:30:29 +0530

    MERN Booking App error -fix 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import db from './config/mongoose.js';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.js'
-import seatRoutes from './routes/seatRoutes.js'
+import SeatRoutes from './routes/SeatRoutes.js'
 
 dotenv.config();
 db();
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
 app.use('/api/users',userRoutes);
-app.use('/api/seats',seatRoutes);
+app.use('/api/seats',SeatRoutes);
 app.listen(port,()=>{
     console.log('listening on port '+port);
 });
